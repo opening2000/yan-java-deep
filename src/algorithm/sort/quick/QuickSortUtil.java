786d480b45ecf09acee4173627eb25ac401938a8
opening2000 <yankj12@163.com>
2014-10-10 17:46:32 +0800

    algorithm-2
    quick sort

---
+++ src/algorithm/sort/quick/QuickSortUtil.java
@@ -0,0 +1,30 @@
+package algorithm.sort.quick;
+
+public class QuickSortUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		testQuickSort();
+
+	}
+
+	public static void testQuickSort(){
+		
+	}
+	
+	public static void quickSort(int[] ary){
+		
+	}
+	
+	/***
+	 * 
+	 * @param ary需要排序的数组
+	 * @param s 需要配需的起始下标
+	 * @param t 需要排序的终止下标
+	 */
+	public static void quickSort(int ary , int s , int t){
+		
+	}
+}
