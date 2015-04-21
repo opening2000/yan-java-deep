5176e9790481dcb2ce22059c2cbe10be2e48b646
opening2000 <yankj12@163.com>
2015-04-21 21:47:39 +0800

    nowcoder consecutive integer sum simple solution

--- src/nowcoder/question/algorithm/ConsecutiveIntegerSum.java
+++ src/nowcoder/question/algorithm/ConsecutiveIntegerSum.java
@@ -3,15 +3,21 @@ package nowcoder.question.algorithm;
 public class ConsecutiveIntegerSum {
 
 	public static void main(String[] args) {
-		
+		devideIntoConsecutiveIntegerSum1(10000);
 	}
 	
-	public static void devideIntoConsecutiveIntegerSum(int a){
-		int i = 2;
-		while(true){
-			int tmp = a / i;
-			
-			i++;
+	public static void devideIntoConsecutiveIntegerSum1(int num){
+		int a1 = 1;
+		int n = 2;
+		for(n = 2 ; n < num ;n++){
+			for(a1 = 1; a1 < num ;a1++){
+				if((n*n + (2*a1 - 1)*n )  == 2 *num){
+					for(int i=1;i<=n;i++){
+						System.out.print((a1 + (i-1)) + ",");
+					}
+					System.out.println();
+				}
+			}
 		}
 	}
 }
