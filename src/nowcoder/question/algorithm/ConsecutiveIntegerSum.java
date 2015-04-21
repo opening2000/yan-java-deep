9b653baa28abc78777d8a8513335cc26fbce0c6d
opening2000 <yankj12@163.com>
2015-04-21 17:41:47 +0800

    nowcoder consecutive integer sum

---
+++ src/nowcoder/question/algorithm/ConsecutiveIntegerSum.java
@@ -0,0 +1,17 @@
+package nowcoder.question.algorithm;
+
+public class ConsecutiveIntegerSum {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void devideIntoConsecutiveIntegerSum(int a){
+		int i = 2;
+		while(true){
+			int tmp = a / i;
+			
+			i++;
+		}
+	}
+}
