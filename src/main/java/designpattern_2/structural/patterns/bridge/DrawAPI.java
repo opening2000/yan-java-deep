9bcbf10b4ff192bc00ad3bad4d19a81f5d1ce90b
yankj12 <yankj12@163.com>
2018-08-23 23:09:57 +0800

    结构模式/桥接模式

---
+++ src/main/java/designpattern_2/structural/patterns/bridge/DrawAPI.java
@@ -0,0 +1,6 @@
+package designpattern_2.structural.patterns.bridge;
+
+
+public interface DrawAPI {
+   public void drawCircle(int radius, int x, int y);
+}
---
+++ src/main/java/designpattern_2/structural/patterns/bridge/RedCircle.java
@@ -0,0 +1,10 @@
+package designpattern_2.structural.patterns.bridge;
+
+
+public class RedCircle implements DrawAPI {
+   @Override
+   public void drawCircle(int radius, int x, int y) {
+      System.out.println("Drawing Circle[ color: red, radius: "
+         + radius +", x: " +x+", "+ y +"]");
+   }
+}
