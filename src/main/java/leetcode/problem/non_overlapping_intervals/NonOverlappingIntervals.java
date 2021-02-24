c43fa76ae6337c75e3d7acd538a9db7fa1890d03
yankj12 <yankj12@163.com>
2021-02-24 18:57:21 +0800

    leetcode   non-overlapping-intervals

---
+++ src/main/java/leetcode/problem/non_overlapping_intervals/NonOverlappingIntervals.java
@@ -0,0 +1,26 @@
+package leetcode.problem.non_overlapping_intervals;
+
+/**
+ * https://leetcode-cn.com/problems/non-overlapping-intervals/
+ * @author Yankj
+ *
+ */
+public class NonOverlappingIntervals {
+
+	public static void main(String[] args) {
+		// 输入: [ [1,2], [2,3], [3,4], [1,3] ]
+		// 输出: 1
+		int[][] intervals= new int[][] { {1,2}, {2,3}, {3,4}, {1,3} };
+		System.out.println();
+	}
+
+}
+
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+    	int removeCount = 0;
+    	
+    	return removeCount;
+    }
+}
